191eb8b980c883e4eba4fe31fd4d64cde22ae2bd
Simon Hudon <simon.hudon@gmail.com>
2019-04-21 11:28:19 -0400

    address comments

--- src/tactic/default.lean
+++ src/tactic/default.lean
@@ -5,3 +5,5 @@ import
   tactic.mk_iff_of_inductive_prop
   tactic.wlog
   tactic.monotonicity.interactive
+  tactic.basic tactic.rcases tactic.generalize_proofs
+  tactic.split_ifs tactic.ext tactic.tauto
